28db31cf625f928326f843ec9ce7dc44817567b0
Cooper Saye <coopersaye@gmail.com>
2020-10-19 21:55:16 -0700

    Implement Square class.

---
+++ Assets/Powerbox/Scripts/Square.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Powerbox
+{
+    public enum SquareType
+    {
+        Empty,
+        Node,
+        Wire
+    }
+
+    public enum SquareColor
+    {
+        None,
+        Red,
+        Green,
+        Blue
+    }
+
+    [Serializable]
+    public class Square
+    {
+        public SpriteRenderer spriteRenderer;
+        public SquareType type;
+        public SquareColor color;
+
+        public Square(SpriteRenderer _spriteRenderer, SquareType _type, SquareColor _color)
+        {
+            spriteRenderer = _spriteRenderer;
+            type = _type;
+            color = _color;
+        }
+    }
+}
