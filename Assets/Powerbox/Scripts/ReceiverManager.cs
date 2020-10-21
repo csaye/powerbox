f0349ef19b46568866e25e25ffd27cb742bc208a
Cooper Saye <coopersaye@gmail.com>
2020-10-21 13:17:59 -0700

    Import receiver sprites.

---
+++ Assets/Powerbox/Scripts/ReceiverManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Powerbox
+{
+    public class ReceiverManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Sprite[] receiverSprites = new Sprite[4];
+
+        private void Update()
+        {
+            
+        }
+    }
+}
